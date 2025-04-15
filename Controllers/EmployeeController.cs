a98872e30d9d65f6c8c466124a83cb7f8a3c66cb
Pakizasaal <psadat31@gmail.com>
2025-04-15 15:46:49 +0200

    Opgave G: Tilføjet endpoint for filtrering på afdeling

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -8,6 +8,18 @@ namespace IBASEmployeeService.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly ILogger<EmployeeController> _logger;
+        
+        private static readonly List<Employee> employees = new()
+        {
+            new Employee { Id = "21", Name = "Mette Bangsbo", Email = "meba@ibas.dk", Department = new Department { Id = 1, Name = "Salg" } },
+            new Employee { Id = "22", Name = "Hans Merkel", Email = "hame@ibas.dk", Department = new Department { Id = 2, Name = "Support" } },
+            new Employee { Id = "23", Name = "Karsten Mikkelsen", Email = "kami@ibas.dk", Department = new Department { Id = 2, Name = "Support" } },
+            new Employee { Id = "24", Name = "Peter Andersen", Email = "peter@ibas.dk", Department = new Department { Id = 3, Name = "IT" } },
+            new Employee { Id = "25", Name = "Cecilie Pedersen", Email = "cecilie@ibas.dk", Department = new Department { Id = 3, Name = "IT" } },
+            new Employee { Id = "26", Name = "Andreas Holm", Email = "andreas@ibas.dk", Department = new Department { Id = 3, Name = "IT" } },
+            new Employee { Id = "27", Name = "Lotte Madsen", Email = "lotte@ibas.dk", Department = new Department { Id = 4, Name = "Kantinen" } },
+            new Employee { Id = "28", Name = "Omar Saleh", Email = "omar@ibas.dk", Department = new Department { Id = 4, Name = "Kantinen" } }
+        };
         public EmployeeController(ILogger<EmployeeController> logger)
         {
             _logger = logger;
@@ -17,92 +29,13 @@ namespace IBASEmployeeService.Controllers
         [HttpGet("GetEmployees")]
         public IEnumerable<Employee> Get()
         {
-            var employees = new List<Employee>() {
-                new Employee() {
-                    Id = "21",
-                    Name = "Mette Bangsbo",
-                    Email = "meba@ibas.dk",
-                    Department = new Department() {
-                        Id = 1,
-                        Name = "Salg"
-                    }
-                },
-                new Employee() {
-                    Id = "22",
-                    Name = "Hans Merkel",
-                    Email = "hame@ibas.dk",
-                    Department = new Department() {
-                        Id = 2,
-                        Name = "Support"
-                    }
-                },
-                new Employee() {
-                    Id = "23",
-                    Name = "Karsten Mikkelsen",
-                    Email = "kami@ibas.dk",
-                    Department = new Department() {
-                        Id = 2,
-                        Name = "Support"
-                    }
-                },
-                new Employee()
-                {
-                    Id = "24",
-                    Name = "Peter Andersen",
-                    Email = "peter@ibas.dk",
-                    Department = new Department()
-                    {
-                        Id = 3,
-                        Name = "IT"
-                    }
-                },
-                new Employee()
-                {
-                    Id = "25",
-                    Name = "Cecilie Pedersen",
-                    Email = "cecilie@ibas.dk",
-                    Department = new Department()
-                    {
-                        Id = 3,
-                        Name = "IT"
-                    }
-                },
-                new Employee()
-                {
-                    Id = "26",
-                    Name = "Andreas Holm",
-                    Email = "andreas@ibas.dk",
-                    Department = new Department()
-                    {
-                        Id = 3,
-                        Name = "IT"
-                    }
-                },
-                new Employee()
-                {
-                    Id = "27",
-                    Name = "Lotte Madsen",
-                    Email = "lotte@ibas.dk",
-                    Department = new Department()
-                    {
-                        Id = 4,
-                        Name = "Kantinen"
-                    }
-                },
-                new Employee()
-                {
-                    Id = "28",
-                    Name = "Omar Saleh",
-                    Email = "omar@ibas.dk",
-                    Department = new Department()
-                    {
-                        Id = 4,
-                        Name = "Kantinen"
-                    }
-                }
-                
-            };
-            return employees;
+           return employees;
+        }
+        
+        [HttpGet("department/{departmentName}")]
+        public IEnumerable<Employee> GetByDepartment(string departmentName)
+        {
+            return employees.Where(e => e.Department.Name.ToLower() == departmentName.ToLower());
         }
     }
 
